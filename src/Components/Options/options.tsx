5421e3be0ff1cb42b12fa9deacc4d754184f44a2
Mark Sorial <msorial44@gmail.com>
2022-05-16 23:42:24 +0400

    fixed key recording bug

--- src/Components/Options/options.tsx
+++ src/Components/Options/options.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import logo from './logo.svg';
 import { Button } from 'antd';
 import 'antd/dist/antd.css';
@@ -9,33 +9,54 @@ export default function Options(props: any) {
     const [canRecord, setCanRecord] = useState(false);
     const [canDelete, setCanDelete] = useState(false);
     const [canSpecial, setCanSpecial] = useState(false);
+
     const [recordKey, setRecordKey] = useState('N/A');
     const [deleteKey, setDeleteKey] = useState('N/A');
     const [specialKey, setSpecialKey] = useState('N/A');
 
-    window.addEventListener('keydown', (event) => {
-        if (canRecord) {
-            setRecordKey(event.key);
-            setCanRecord(false);
-        } else if (canDelete) {
-            setDeleteKey(event.key);
-            setCanDelete(false);
-        } else if (canSpecial) {
-            setSpecialKey(event.key);
-            setCanSpecial(false);
-        }
-    });
+    const [recordButtonContent, setRecordButtonContent] = useState('Click to Record Keybind');
+    const [deleteButtonContent, setDeleteButtonContent] = useState('Click to Record Keybind');
+    const [specialButtonContent, setSpecialButtonContent] = useState('Click to Record Keybind');
+    
+    const handleRecordClick = useCallback((event: { key: React.SetStateAction<string>; }) => {
+      if (canRecord) {
+        setRecordKey(event.key);
+        setCanRecord(false);
+        setRecordButtonContent('Click to Record Keybind');
+      } else if (canDelete) {
+        setDeleteKey(event.key);
+        setCanDelete(false);
+        setDeleteButtonContent('Click to Record Keybind');
+      } else if (canSpecial) {
+        setSpecialKey(event.key);
+        setCanSpecial(false);
+        setSpecialButtonContent('Click to Record Keybind');
+      }
+      setCanRecord(false);
+      setCanDelete(false);
+      setCanSpecial(false);
+    }, [canRecord, canDelete, canSpecial]);
+
+    useEffect(() => {
+      window.addEventListener('keydown', handleRecordClick);
+      return () => window.removeEventListener('keydown', handleRecordClick);
+
+    }, [handleRecordClick]);
 
     function handleRecord() {
+      console.log('clicked record')
         setCanRecord(true);
+        setRecordButtonContent('Recording...');
     }
 
     function handleDelete() {
         setCanDelete(true);
+        setDeleteButtonContent('Recording...');
     }
 
     function handleSpecial() {
         setCanSpecial(true);
+        setSpecialButtonContent('Recording...');
     }
 
 
@@ -48,7 +69,7 @@ export default function Options(props: any) {
               <h2>Record Button</h2>
             </div>
             <div className='record-body'> 
-              <Button type="default" onClick={() => {handleRecord()}}>Click to Record Keybind</Button>
+              <Button type="default" onClick={() => {handleRecord()}}>{recordButtonContent}</Button>
               <div className='curr-record-key'> {recordKey} </div>
             </div>
           </div>
@@ -58,7 +79,7 @@ export default function Options(props: any) {
               <h2>Delete Button</h2>
             </div>
             <div className='delete-body'> 
-              <Button type="default" onClick={() => {handleDelete()}}>Click to Record Keybind</Button>
+              <Button type="default" onClick={() => {handleDelete()}}>{deleteButtonContent}</Button>
               <div className='curr-delete-key'> {deleteKey} </div>
             </div>
           </div>
@@ -68,7 +89,7 @@ export default function Options(props: any) {
               <h2>Special Button</h2>
             </div>
             <div className='special-body'> 
-              <Button type="default" onClick={() => {handleSpecial()}}>Click to Record Keybind</Button>
+              <Button type="default" onClick={() => {handleSpecial()}}>{specialButtonContent}</Button>
               <div className='curr-special-key'> {specialKey} </div>
             </div>
           </div>
